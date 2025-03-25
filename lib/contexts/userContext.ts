14fd04371a1d7cc91c01d069ff924f0ce6856ba1
maxi bernard <bmaximiliano0210@gmail.com>
2025-03-24 22:26:47 -0300

    feat: create default view on user creation

--- lib/contexts/userContext.ts
+++ lib/contexts/userContext.ts
@@ -2,12 +2,23 @@ import { UserJSON } from "@clerk/nextjs/server";
 import { User } from "@prisma/client";
 import db from "../prisma";
 
-// Creates user from Clerk webhook
+// Creates user from Clerk webhook and sets its default view
 export async function createUserFromClerk(data: UserJSON): Promise<User> {
     const user = await db.user.create({
         data: {
             id: data.id
         }
+    });
+    await db.view.create({
+        data: {
+            name: "Default view",
+            userId: user.id,
+            DefaultForUser: {
+                connect: {
+                    id: user.id
+                }
+            }
+        },
     });
     return user;
 }
