75b0f5bfbc309b1dddf7194a8e01119bb748246b
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2025-03-29 02:23:16 -0300

    refactor: delete duplicate file

--- app/actions/sensorActions.ts
+++ app/actions/sensorActions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { getSensorsQty } from "@/lib/contexts/sensorsContext";
+import { getSensorsQty } from "@/lib/contexts/sensorContext";
 import { auth } from "@clerk/nextjs/server";
 
 export async function getSensorsQtyAction(passedUserId: string) {
